4ec8989efd872a609f3698ec9aef84ebd8088d99
jay sung <namic123@naver.com>
2023-10-01 00:07:09 +0900

    @Autowired type examples code

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,18 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class OrderServiceImpl implements OrderService{   // 주문 서비스 구현체
-    private final ClientRepository clientRepository; // 회원 저장소 객체
-    private final DiscountPolicy discountPolicy;  // 할인 정책 인터페이스 (역할)
+public class OrderServiceImpl implements OrderService {   // 주문 서비스 구현체
+    private ClientRepository clientRepository; // 회원 저장소 객체
+    private DiscountPolicy discountPolicy;  // 할인 정책 인터페이스 (역할)
 
-    @Autowired
+//    @Autowired // setter를 통한 의존성 주입
+//    public void setClientRepository(ClientRepository clientRepository) {
+//        this.clientRepository = clientRepository;
+//    }
+//    @Autowired  // setter를 통한 의존성 주입
+//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
+//        this.discountPolicy = discountPolicy;
+//    }
+
+//
+//    @Autowired  // 일반 메서드 주입, 생성자랑 비슷하게 생김
+//    public void init(ClientRepository clientRepository, DiscountPolicy discountPolicy){
+//        this.clientRepository = clientRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
+
+    @Autowired  // 생성자를 통한 의존성 주입
     public OrderServiceImpl(ClientRepository clientRepository, DiscountPolicy discountPolicy) {
         this.clientRepository = clientRepository;
         this.discountPolicy = discountPolicy;
     }
 
     // 테스트 용도
-    public ClientRepository getClientRepository(){
+    public ClientRepository getClientRepository() {
         return clientRepository;
     }
 
