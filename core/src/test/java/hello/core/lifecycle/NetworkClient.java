c39dc78cb0956f52356c4600fae3295423b329f1
jay sung <namic123@naver.com>
2023-10-02 20:39:18 +0900

    빈 생명주기 콜백 초기화, 소멸 메소드(@Bean(initMethod, destroyMethod))

--- core/src/test/java/hello/core/lifecycle/NetworkClient.java
+++ core/src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,10 @@
 package hello.core.lifecycle;
 
-import org.springframework.beans.factory.DisposableBean;
-import org.springframework.beans.factory.InitializingBean;
-
 // 자바 코드
 //외부 네트워크에 미리 연결하는 객체를 하나 생성한다고 가정.(실제 네트워크 연결이 아닌 단순히 문자만 출력)
 //* 클라이언트는 애플리케이션 시작 시점 connect()를 호출해서 연결을 맺어두야하고,
 //* 종료시점에 disConnect()를 호출해서 연결을 끊어야 한다고 가정.
-public class NetworkClient implements InitializingBean, DisposableBean {
+public class NetworkClient {
     private String url; // 접속 url
     public NetworkClient(){     // 기본 생성자
         System.out.println("생성자 호출, url = " + url);
@@ -30,15 +27,17 @@ public class NetworkClient implements InitializingBean, DisposableBean {
         System.out.println("close: " + url);
     }
 
-    @Override
-    public void afterPropertiesSet() throws Exception {
+
+    // 관례상 많이 사용되는 이름
+    // 초기화 콜백
+    public void init() {
         // 의존 관계 주입이 끝나면 호출하겠다는 메서드
         connect();
         call("초기화 연결 메시지");
     }
-
-    @Override
-    public void destroy() throws Exception {
+    // 관례상 많이 사용되는 이름
+    // 소멸 콜백
+    public void close() {
      disconnect();
     }
 }
